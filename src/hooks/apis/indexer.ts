b5af0332f21ed45fcf5bcc53b8dd8aa1c62ab8bb
torsten0208 <torsten0208@163.com>
2025-04-25 16:11:23 +0800

    feat: hooks by comments and edit userInfo

--- src/hooks/apis/indexer.ts
+++ src/hooks/apis/indexer.ts
@@ -1,5 +1,6 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useMutation, useQuery } from "@tanstack/react-query";
 
+import { createComment, getCommentList } from "@/apis/comment";
 import {
 	type CandleParameters,
 	getTokenList,
@@ -7,7 +8,9 @@ import {
 	getTokenTransactionList,
 	type TokenListParameters,
 } from "@/apis/indexer";
+import { updateUserInfo } from "@/apis/user-login";
 import { getICPCanisterId } from "@/canisters/icrc3";
+import { useIcIdentityStore } from "@/store/ic";
 
 export const useInfiniteTokenList = (parameters: TokenListParameters) => {
 	return useInfiniteQuery({
@@ -66,5 +69,58 @@ export const useTokenPriceCandle = (parameters: CandleParameters) => {
 		],
 		queryFn: () => getTokenPriceCandle(parameters),
 		refetchInterval: 10000,
+	});
+};
+
+export const useTokenComments = (parameters: { meme_token_id: string }) => {
+	return useInfiniteQuery({
+		queryKey: ["ic-core", "tokenCommentList", parameters.meme_token_id],
+		queryFn: ({ pageParam: pageParameter = 1 }: { pageParam: number }) =>
+			getCommentList({
+				...parameters,
+				page: pageParameter,
+			}),
+		getNextPageParam: (lastPage, pages) => {
+			return lastPage.total > pages.length ? pages.length + 1 : undefined;
+		},
+		initialPageParam: 1,
+	});
+};
+
+// ============== Mutations ================
+export type SendCommentArgs = {
+	tokenId: string;
+	content: string;
+	photo?: string;
+};
+export const useCreateTokenComment = (parameters: { tokenId: string }) => {
+	const { jwt_token } = useIcIdentityStore();
+	return useMutation({
+		mutationKey: ["ic-core", "sendComment", parameters.tokenId],
+		mutationFn: async (args: SendCommentArgs) => {
+			return createComment(jwt_token, {
+				tokenId: Number(args.tokenId),
+				content: args.content,
+				photo: args.photo,
+			});
+		},
+	});
+};
+
+// update user info
+export type EditInfoArgs = {
+	userToken: string;
+	name: string;
+	avatar?: string;
+};
+export const useUpdateUserInfo = () => {
+	return useMutation({
+		mutationKey: ["ic-core", "updateUserInfo"],
+		mutationFn: async (args: EditInfoArgs) => {
+			return updateUserInfo(args.userToken, {
+				name: args.name,
+				avatar: args.avatar,
+			});
+		},
 	});
 };
