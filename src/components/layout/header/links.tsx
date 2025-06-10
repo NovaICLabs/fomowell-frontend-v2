b20c556380b2196cea9d6c64517adbaaa7b6c073
torsten0208 <torsten0208@163.com>
2025-06-10 19:30:31 +0800

    feat: update menu sort

--- src/components/layout/header/links.tsx
+++ src/components/layout/header/links.tsx
@@ -10,13 +10,13 @@ export const menuLinks = [
 		label: "Tokens",
 		to: "/",
 	},
-	{
-		label: "Referral",
-		to: "/referral",
-	},
 	// {
 	// 	label: "Liquidity",
 	// 	to: "/liquidity",
+	// },
+	// {
+	// 	label: "Referral",
+	// 	to: "/referral",
 	// },
 ];
 
@@ -34,16 +34,33 @@ export default function Links() {
 					label: "Liquidity",
 					to: "/bitcoin/liquidity",
 				},
+				{
+					label: "Referral",
+					to: "/referral",
+				},
 			];
 			return newMenus;
 		}
 
-		return menuLinks;
+		const newMenus = [
+			...menuLinks,
+			{
+				label: "Referral",
+				to: "/referral",
+			},
+		];
+
+		return newMenus;
+	}, [chain]);
+
+	const menuLeftCount = useMemo(() => {
+		return chain === "bitcoin" ? 1 : 0;
 	}, [chain]);
 
 	return (
 		<div className="flex items-center gap-[20px]">
-			{menus.map((link) => {
+			{menus.map((link, index) => {
+				if (index > menuLeftCount) return;
 				const isActive = location.pathname === link.to;
 				return (
 					<Link
@@ -74,6 +91,31 @@ export default function Links() {
 			>
 				How it works
 			</div>
+			{menus.map((link, index) => {
+				if (index <= menuLeftCount) return;
+
+				const isActive = location.pathname === link.to;
+				return (
+					<Link
+						key={link.to}
+						to={link.to}
+						className={`relative text-sm font-medium ${
+							isActive ? "text-white" : "text-white/60 hover:text-white"
+						}`}
+					>
+						{link.label}
+						<div
+							className={`absolute -bottom-1 left-0 h-[1px] rounded-[1px] transition-all duration-300 ease-in-out ${
+								isActive ? "w-full opacity-100" : "w-0 opacity-0"
+							}`}
+							style={{
+								background:
+									"linear-gradient(90deg, #F7B406 0%, rgba(247, 180, 6, 0.00) 100%)",
+							}}
+						/>
+					</Link>
+				);
+			})}
 		</div>
 	);
 }
